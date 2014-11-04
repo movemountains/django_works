20ca2ebd0f02e00e7344dd5b8033cf0e34d7f184
movemountains <movemountains4321@gmail.com>
2014-11-04 14:54:15 -0800

    django tutorial

---
+++ Envs/tutorial/mysite/polls/static/polls/bootstrap/js/style.js
@@ -0,0 +1,8 @@
+/**
+ * Created by kalyani on 10/17/14.
+ */
+$(document).ready(function(){
+    $("").hover(function () {
+        $("a").css("background-color", "green");
+    });
+    });
---
+++ Envs/tutorial/mysite/polls/static/polls/bootstrap/js/tip.js
@@ -0,0 +1,5 @@
+/**
+ * Created by kalyani on 10/16/14.
+ */
+var d = new Date();
+document.getElementById("demo").innerHTML = d.toDateString();
